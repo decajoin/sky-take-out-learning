84caa1048a214968af403dd2f6ed1f84bfc18171
acer <acer@acer.com>
2024-12-14 15:51:34 +0800

    Redis 数据库配置

---
+++ sky-take-out/sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,26 @@
+package com.sky.config;
+
+import io.lettuce.core.dynamic.RedisCommandFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        log.info("开始创建 RedisTemplate 对象");
+
+        RedisTemplate redisTemplate = new RedisTemplate();
+        // 设置 Redis 连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // 设置 key 的序列化方式
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
