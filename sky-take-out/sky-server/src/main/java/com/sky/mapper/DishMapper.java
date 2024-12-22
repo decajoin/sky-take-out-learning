1894db6d1bdb611a8a1c615920c2d4b19d29d722
acer <acer@acer.com>
2024-12-22 13:48:18 +0800

    工作台模块业务代码开发（Copy）

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -57,9 +58,16 @@ public interface DishMapper {
 
     /**
      * 根据套餐 id 查询菜品
-     * @param setmealId
+     * @param id
      * @return
      */
     @Select("select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
     List<Dish> getBySetmealId(Long id);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
